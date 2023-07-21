6746c1960cef53da126f4a4a04fb4c64265d5939
Benjamin Kirk Sandland <bks@deif.com>
2023-07-21 09:36:07 +0200

    add specs testing area functioniality

--- spec/win32/screenshot/take_spec.rb
+++ spec/win32/screenshot/take_spec.rb
@@ -16,12 +16,36 @@ describe Win32::Screenshot::Take do
     expect([image.width, image.height]).to eq(Win32::Screenshot::BitmapMaker.dimensions_for(hwnd))
   end
 
+  it "captures an area of the foreground" do
+    image = Win32::Screenshot::Take.of(:foreground, :area => [30, 30, 100, 150])
+    save_and_verify_image(image, 'foreground_area')
+    expect(image.width).to eq(70)
+    expect(image.height).to eq(120)
+  end
+
+  it "doesn't allow to capture an area of the foreground with invalid coordinates" do
+    expect {Win32::Screenshot::Take.of(:foreground, :area => [0, 0, -1, 100])}.
+      to raise_exception("specified coordinates (x1: 0, y1: 0, x2: -1, y2: 100) are invalid - cannot be negative!")
+  end
+
   it "captures the desktop" do
     image = Win32::Screenshot::Take.of(:desktop)
     desktop_dimensions = Win32::Screenshot::BitmapMaker::desktop
     save_and_verify_image(image, 'desktop')
 
     expect([image.width, image.height]).to eq([desktop_dimensions.width, desktop_dimensions.height])
+  end
+
+  it "captures an area of the desktop" do
+    image = Win32::Screenshot::Take.of(:desktop, :area => [30, 30, 100, 150])
+    save_and_verify_image(image, 'desktop_area')
+    expect(image.width).to eq(70)
+    expect(image.height).to eq(120)
+  end
+
+  it "doesn't allow to capture an area of the desktop with invalid coordinates" do
+    expect {Win32::Screenshot::Take.of(:desktop, :area => [0, 0, -1, 100])}.
+      to raise_exception("specified coordinates (x1: 0, y1: 0, x2: -1, y2: 100) are invalid - cannot be negative!")
   end
 
   it "captures a window" do
@@ -60,6 +84,13 @@ describe Win32::Screenshot::Take do
     expect([image.width, image.height]).to eq(Win32::Screenshot::BitmapMaker.dimensions_for(window.hwnd))
   end
 
+  it "captures an area of the window" do
+    image = Win32::Screenshot::Take.of(:window, :pid => @notepad, :area => [30, 30, 100, 150])
+    save_and_verify_image(image, 'notepad_area')
+    expect(image.width).to eq(70)
+    expect(image.height).to eq(120)
+  end
+
   it "captures by the RAutomation::Window" do
     window = RAutomation::Window.new(:pid => @notepad)
     image = Win32::Screenshot::Take.of(:window, :rautomation => window)
@@ -72,6 +103,37 @@ describe Win32::Screenshot::Take do
     image = Win32::Screenshot::Take.of(:window, :rautomation => window)
     save_and_verify_image(image, 'iexplore_child')
     expect([image.width, image.height]).to eq(Win32::Screenshot::BitmapMaker.dimensions_for(window.hwnd))
+  end
+
+  it "captures a whole window if window size is specified as coordinates" do
+    window = RAutomation::Window.new(:pid => @notepad)
+    expected_width, expected_height = Win32::Screenshot::BitmapMaker.dimensions_for(window.hwnd)
+    image = Win32::Screenshot::Take.of(:window, :rautomation => window, :area => [0, 0, expected_width, expected_height])
+    save_and_verify_image(image, 'notepad_area_full_window')
+    expect(image.width).to eq(expected_width)
+    expect(image.height).to eq(expected_height)
+  end
+
+  it "doesn't allow to capture an area of the window with negative coordinates" do
+    expect {Win32::Screenshot::Take.of(:window, :pid => @notepad, :area => [0, 0, -1, 100])}.
+      to raise_exception("specified coordinates (x1: 0, y1: 0, x2: -1, y2: 100) are invalid - cannot be negative!")
+  end
+
+  it "doesn't allow to capture an area of the window if coordinates are the same" do
+    expect {Win32::Screenshot::Take.of(:window, :pid => @notepad, :area => [10, 0, 10, 20])}.
+      to raise_exception("specified coordinates (x1: 10, y1: 0, x2: 10, y2: 20) are invalid - cannot be x1 >= x2 or y1 >= y2!")
+  end
+
+  it "doesn't allow to capture an area of the window if second coordinate is smaller than first one" do
+    expect {Win32::Screenshot::Take.of(:window, :pid => @notepad, :area => [0, 10, 10, 9])}.
+      to raise_exception("specified coordinates (x1: 0, y1: 10, x2: 10, y2: 9) are invalid - cannot be x1 >= x2 or y1 >= y2!")
+  end
+
+  it "doesn't allow to capture an area of the window with too big coordinates" do
+    window = RAutomation::Window.new(:pid => @notepad)
+    expected_width, expected_height = Win32::Screenshot::BitmapMaker.dimensions_for(window.hwnd)
+    expect {Win32::Screenshot::Take.of(:window, :pid => @notepad, :area => [0, 0, 10, 100000])}.
+      to raise_exception("specified coordinates (x1: 0, y1: 0, x2: 10, y2: 100000) are invalid - maximum x2: #{expected_width} and y2: #{expected_height}!")
   end
 
   after :all do
